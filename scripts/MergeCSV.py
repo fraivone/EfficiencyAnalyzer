def5185697f58c3406059cb8ffd8f4cad9b4ddcf
Laurent Pétré <laurent.petre@cern.ch>
2024-05-14 17:34:53 +0200

    Reference MergeCSV.py output from OUTPUT_PATH rather than from BASE_DIR

--- scripts/MergeCSV.py
+++ scripts/MergeCSV.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import argparse
 import time
-from Utils import BASE_DIR, mergeEfficiencyCSV# concat multiple dfs
+from Utils import OUTPUT_PATH, mergeEfficiencyCSV
 
 ##### Parser
 parser = argparse.ArgumentParser(description='Merge csv')
@@ -13,6 +13,6 @@ output_name = args.output_name+time.strftime("_%-y%m%d%H%M")
 
 # concat multiple dfs
 final_df = mergeEfficiencyCSV([pd.read_csv(k, sep=',') for k in args.inputs])
+
 # save to file
-final_df.to_csv(BASE_DIR /f"data/output/{output_name}.csv",index=False)
-with open(BASE_DIR /f"data/output/{output_name}.yml", "w") as text_file: text_file.writelines(line + '\n' for line in args.inputs)
+final_df.to_csv(OUTPUT_PATH / f"/{output_name}.csv", index=False)
